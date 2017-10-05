87f78645b20fb4d9122c6501d4fc73a763dbb268
Eddy Luten <eddyluten@gmail.com>
2017-10-05 13:23:03 -0700

    PID file support

--- lowpoly_bot.py
+++ lowpoly_bot.py
@@ -1,65 +1,84 @@
 #!/usr/bin/env python3
+import os
+import sys
 import tweepy
 from time import sleep
 from credentials import *
 
-tweet_interval = 20
-other_interval = 5
+pid = str(os.getpid())
+pidfile = '/tmp/lowpoly_bot.pid'
 
-# Access and authorize our Twitter credentials from credentials.py
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+# Check if the process is already running, and if so, exit.
+if os.path.isfile(pidfile):
+    print('process already running, exiting...')
+    sys.exit()
 
-import sqlite3
-conn = sqlite3.connect('lowpoly_bot.db')
-cursor = conn.cursor()
+# If not, write the pid file
+open(pidfile, 'w').write(pid)
 
-cursor.execute("CREATE TABLE IF NOT EXISTS processed (tweet_id UNSIGNED BIG INT)")
-conn.commit()
+def bot_main():
+    tweet_interval = 20
+    other_interval = 5
 
-spammers = open('spammers', 'r').read().split(',')
+    # Access and authorize our Twitter credentials from credentials.py
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
 
-for tweet in tweepy.Cursor(api.search, q='#lowpoly -filter:retweets filter:media', result_type='recent').items():
+    import sqlite3
+    conn = sqlite3.connect('lowpoly_bot.db')
     cursor = conn.cursor()
 
-    if tweet.user.screen_name in spammers:
-        print('Ignoring spammer ' + tweet.user.screen_name)
-        continue
+    cursor.execute("CREATE TABLE IF NOT EXISTS processed (tweet_id UNSIGNED BIG INT)")
+    conn.commit()
 
-    cursor.execute('SELECT tweet_id FROM processed WHERE tweet_id = ?', (tweet.id,))
-    if None is not cursor.fetchone():
-        print('.', end='', flush=True)
-        continue
+    spammers = open('spammers', 'r').read().split(',')
 
-    try:
-        # Don't retweet retweets and quotes
-        if tweet.retweeted or tweet.is_quote_status:
+    for tweet in tweepy.Cursor(api.search, q='#lowpoly -filter:retweets filter:media', result_type='recent').items():
+        cursor = conn.cursor()
+
+        if tweet.user.screen_name in spammers:
+            print('Ignoring spammer ' + tweet.user.screen_name)
             continue
 
-        # Retweet tweets as they are found
-        tweet.retweet()
-        print('Retweeted ' + str(tweet.id) + ' by ' + tweet.user.screen_name)
+        cursor.execute('SELECT tweet_id FROM processed WHERE tweet_id = ?', (tweet.id,))
+        if None is not cursor.fetchone():
+            print('.', end='', flush=True)
+            continue
 
-        cursor.execute('INSERT INTO processed (tweet_id) VALUES (?)', (tweet.id,))
-        conn.commit()
+        try:
+            # Don't retweet retweets and quotes
+            if tweet.retweeted or tweet.is_quote_status:
+                continue
 
-        # Follow the user if we retweeted their stuff
-        if not tweet.user.following:
-            print('Followed ' + tweet.user.screen_name)
-            tweet.user.follow()
+            # Retweet tweets as they are found
+            tweet.retweet()
+            print('Retweeted ' + str(tweet.id) + ' by ' + tweet.user.screen_name)
 
-        print('')
-        sleep(tweet_interval)
-    except tweepy.TweepError as e:
-        print('Error (' + str(tweet.id) + '): ' + e.reason)
+            cursor.execute('INSERT INTO processed (tweet_id) VALUES (?)', (tweet.id,))
+            conn.commit()
 
-        cursor.execute('INSERT INTO processed (tweet_id) VALUES (?)', (tweet.id,))
-        conn.commit()
+            # Follow the user if we retweeted their stuff
+            if not tweet.user.following:
+                print('Followed ' + tweet.user.screen_name)
+                tweet.user.follow()
 
-        sleep(other_interval)
-    except StopIteration:
-        break
+            print('')
+            sleep(tweet_interval)
+        except tweepy.TweepError as e:
+            print('Error (' + str(tweet.id) + '): ' + e.reason)
 
-conn.close()
-print('')
+            cursor.execute('INSERT INTO processed (tweet_id) VALUES (?)', (tweet.id,))
+            conn.commit()
+
+            sleep(other_interval)
+        except StopIteration:
+            break
+
+    conn.close()
+    print('')
+
+try:
+    bot_main()
+finally:
+    os.unlink(pidfile)
